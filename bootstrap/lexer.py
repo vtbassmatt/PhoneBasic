dcb72992556e3c524b993e597a1f3aa307d3d515
Matt Cooper <vtbassmatt@gmail.com>
2014-05-07 06:44:07 -0700

    full lexer for the language

--- bootstrap/lexer.py
+++ bootstrap/lexer.py
@@ -14,10 +14,14 @@ def tokenize(s):
         'AS', 'ACCEPT', 'RETURN', 'CLEAR', 'END'}
     token_specification = [
         ('NUMBER',  r'\d+(\.\d*)?'), # Integer or decimal number
+        ('STRING',  r'"([^"])*"'),   # Simple strings (no escape character)
         ('ASSIGN',  r'BE'),          # Assignment operator
         ('ID',      r'[A-Za-z][A-Za-z0-9_]*'),  # Identifiers
         ('COMMENT', r'\/\/.*'),      # Comments
-        ('OP',      r'[+*\/\-]'),    # Arithmetic operators
+        ('ARITHOP', r'[+*\/\-]'),    # Arithmetic operators
+        ('COMPOP',  r'<|<=|=|!=|=>|>'),    # Comparison operators
+        ('COLON',   r':'),           # Colon (as in labels)
+        ('COMMA',   r','),           # Comma (as in expression lists)
         ('NEWLINE', r'\n'),          # Line endings
         ('SKIP',    r'[ \t]'),       # Skip over spaces and tabs
     ]
@@ -36,6 +40,9 @@ def tokenize(s):
             val = mo.group(typ)
             if typ == 'ID' and val in keywords:
                 typ = val
+            if typ == 'STRING':
+                # remove the surrounding quotes
+                val = val[1:-1]
             yield Token(typ, val, line, mo.start()-line_start)
         pos = mo.end()
         mo = get_token(s, pos)
@@ -45,10 +52,37 @@ def tokenize(s):
 
 if __name__ == "__main__":
     statements = '''
-        IF quantity THEN
-            LET total BE total + price * quantity
-            LET tax BE price * 0.05
-        ENDIF
+// We use the 'LET var BE' syntax because = is buried on phone keypads
+LET A BE 1
+LET B BE 0
+LET GREETING BE "Hello world!"
+PRINT GREETING
+
+FirstLoop:
+ PRINT ">"
+ INPUT B
+ PRINT B, " - 1 IS ", B - A
+IF B IS 0 GOTO FirstLoop
+
+SecondLoop:
+LET I BE 1
+  PRINT I
+  LET I BE I + 1
+IF I < 10 GOTO SecondLoop
+
+// We could have chosen 'IF B IS 0' to avoid the =
+// but that looked ugly
+IF B = 0 THEN PRINT "The world makes sense"
+
+COMPUTE C AS Plus2 4
+// this will print '6'
+PRINT C
+
+END
+
+Plus2:
+ ACCEPT Var
+RETURN Var + 2
 '''
 
     for token in tokenize(statements):

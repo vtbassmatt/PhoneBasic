9d16051fe02f02905165fa451416e3e26bedcb18
Matt Cooper <vtbassmatt@gmail.com>
2014-05-09 06:53:20 -0700

    first bit of codegen

--- bootstrap/translator.py
+++ bootstrap/translator.py
@@ -2,9 +2,50 @@ import pprint
 from lexer import tokenize
 from parser import parse
 
-def translate(ast):
-    # TODO: write this
+
+class TranslatorError(RuntimeError):
     pass
+
+
+class Opcode(object):
+    NOOP        = 0
+
+    # screen stuff
+    CLEAR       = 1
+    PRINTNUM    = 2
+    PRINTSTR    = 3
+
+    # flow control
+    GOTO        = 10
+    HALT        = 11
+
+    # working with data
+    LITERAL     = 20
+
+
+def translate(ast):
+    label_table = {}
+    string_table = {}
+    number_table = {}
+    # by convention, put a magic number at the beginning
+    # for this case, "PB01" in ASCII
+    code = [ord("P"), ord("B"), ord("0"), ord("1")]
+
+    for line in ast:
+        if line[0] == 'clear':
+            code.append(Opcode.CLEAR)
+
+        elif line[0] == 'label':
+            label = line[1]
+            if label in label_table:
+                raise TranslatorError("Label already exists", label)
+            label_table[label] = len(code)
+
+        else:
+            code.append(Opcode.NOOP)
+
+    return code
+
 
 if __name__ == "__main__":
     program_text = """CLEAR
